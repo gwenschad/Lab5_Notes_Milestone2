f97cba9c70ebad8460eda42a3a405a4d619bb486
Gwen Schad <gschad@wisc.edu>
2021-10-18 16:09:31 -0500

    added functional logout menu item

--- app/src/main/java/com/example/lab5_notes/MainActivity2.java
+++ app/src/main/java/com/example/lab5_notes/MainActivity2.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
@@ -18,5 +21,32 @@ public class MainActivity2 extends AppCompatActivity {
         Intent intent = getIntent();
         String str = intent.getStringExtra("message");
         welcomeMessage.setText("Welcome " +str +"!");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //handle item selection
+        switch(item.getItemId()) {
+            case R.id.logout:
+                goToActivity1();
+                return true;
+            case R.id.addNote:
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void goToActivity1() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
